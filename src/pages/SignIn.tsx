a1893851c145ae640f7b7f36b91cb890dacf37e3
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-11-27 15:56:48 -0300

    style: :lipstick: Estilizando a página de login
    Estilizando a página de login

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
-
-import { Box, Image } from '@chakra-ui/react';
+import { HiOutlineKey, HiOutlineLockClosed, HiOutlineMail } from "react-icons/hi";
+import { Box, Image, Input, InputGroup, InputLeftElement, Divider } from '@chakra-ui/react';
 import logo from '../assets/images/img/logo.png';
 import { AuthContext } from '../contexts/auth.context';
 
@@ -16,33 +16,46 @@ export function SignIn() {
     }
 
     return (
-        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <Box className='flex justify-center items-center'>
-                <Image src={logo} className='w-8' /> <span className='font-handwrite  ml-2'>Vipera</span>
+        <div className="w-full self-center rounded-md max-w-sm p-4 border border-gray-100 shadow-xl sm:p-6 md:p-8">
+            <Divider />
+            <Box className='flex justify-center items-center rounded-md py-3 select-none'>
+                <Image src={logo} className='w-10 mr-1' /> <span className='font-handwrite ml-2 text-4xl text-gray-600'>Vipera</span>
             </Box>
+            <Divider />
             <form className="space-y-6" onSubmit={e => handleSignIn(e)}>
-                <h5 className="text-xl font-medium text-gray-900 dark:text-white">Login</h5>
-                <div>
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seu email</label>
-                    <input type="email" name="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="nome@email.com" required />
+                <div className='flex justify-center items-center mt-4'>
+                    <HiOutlineLockClosed className='inline mr-1' />
+                    <h5 className="text-md font-medium text-text">Login</h5>
                 </div>
                 <div>
-                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sua senha</label>
-                    <input type="password" name="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                    <InputGroup
+                        className='bg-slate-200 rounded-md'
+                    >
+                        <InputLeftElement
+                            pointerEvents='none'
+                            children={<HiOutlineMail color='gray.300' />}
+                        />
+                        <Input type="email" name="email" id="email" placeholder='email@email.com' value={email} onChange={(event) => setEmail(event.target.value)} className="focus:outline-primary-700 focus:border-none text-text" />
+                    </InputGroup>
+                </div>
+                <div>
+                    <InputGroup
+                        className='bg-slate-200 rounded-md'
+                    >
+                        <InputLeftElement
+                            pointerEvents='none'
+                            children={<HiOutlineKey color='gray.300' />}
+                        />
+                        <Input type="password" name="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="••••••••" className="focus:outline-primary-700 focus:border-none text-text bg-slate-50" />
+                    </InputGroup>
                 </div>
                 <div className="flex items-start">
-                    <div className="flex items-start">
-                        <div className="flex items-center h-5">
-                            <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" />
-                        </div>
-                        <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Manter conectado</label>
-                    </div>
-                    <a href="#" className="ml-auto text-sm text-primary-900 hover:underline dark:text-blue-500">Esqueci a senha?</a>
+                    <a href="#" className="ml-auto text-sm text-primary-900 hover:underline">Esqueci a senha?</a>
                 </div>
-                <button type="submit" className="w-full text-white bg-primary-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-primary-900 dark:focus:ring-blue-800">Login to your account</button>
-                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                    Ainda tem conta? <a href="#" className="text-primary-900 hover:underline dark:text-blue-500">Criar uma conta</a>
-                </div>
+                <button type="submit" className="w-full text-white bg-primary-900 hover:bg-primary-800 focus:ring-2 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Entrar</button>
+                {/* <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                    Ainda tem conta? <a href="#" className="text-primary-900 hover:underline ">Criar uma conta</a>
+                </div> */}
             </form>
         </div>
 
